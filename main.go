780e6241f99682e980b2fcbba22acd71d4551a8f
dracarys18 <karthikey.hegde@juspay.in>
2023-11-27 16:13:14 +0530

    fix: add a cursor

--- main.go
+++ main.go
@@ -1,53 +1,60 @@
 package main
 
 import (
+	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
 	rp "rin/libs/rope"
-	"strings"
 )
 
 const width = 1600
 const height = 900
+const textSize = 20
 
-var isFullScreen = false
+type RinState struct {
+	X            int32
+	Y            int32
+	isFullScreen bool
+}
 
 func free(font rl.Font) {
 	rl.UnloadFont(font)
 	rl.CloseWindow()
 }
 
-func toggleFullScreen() {
-	if isFullScreen {
+func (r *RinState) toggleFullScreen() {
+	if r.isFullScreen {
 		rl.SetWindowSize(width, height)
-		isFullScreen = false
+		r.isFullScreen = false
 	} else {
+		rl.SetWindowPosition(0, 0)
 		rl.SetWindowSize(rl.GetMonitorWidth(rl.GetCurrentMonitor()), rl.GetMonitorHeight(rl.GetCurrentMonitor()))
-		isFullScreen = true
+		r.isFullScreen = true
 	}
 }
-func handleKeyPressed(key int32, rope *rp.Rope) {
+func (r *RinState) handleKeyPressed(key int32, rope *rp.Rope) {
 	switch key {
 	case rl.KeyEnter:
-		toggleFullScreen()
+		r.toggleFullScreen()
 	default:
 		if (key >= 32) && (key <= 125) {
 			str := string(rune(key))
-			if !(rl.IsKeyDown(rl.KeyCapsLock) || rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift)) {
-				str = strings.ToLower(str)
-			}
 			newRope := rp.NewRope(str)
+			cursorWidth := rl.MeasureText(str, textSize)
+			fmt.Println(cursorWidth)
+			r.X += cursorWidth
 			*rope = rope.Concatenate(newRope)
 		}
 	}
 }
+
 func main() {
 	rope := rp.Rope{}
 
+	r := RinState{X: 10, Y: 10}
 	rl.InitWindow(width, height, "Rin")
 
 	font := rl.LoadFontEx("/Users/karthikey.hegde/Library/Fonts/HasklugNerdFontMono-Bold.otf", 20, nil)
 
-	rl.SetWindowPosition(0, 0)
 	rl.SetConfigFlags(rl.FlagMsaa4xHint)
 	defer free(font)
 	rl.SetTargetFPS(60)
@@ -56,8 +63,13 @@ func main() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.Black)
-		handleKeyPressed(rl.GetKeyPressed(), &rope)
-		rl.DrawTextEx(font, rope.String(), rl.Vector2{X: 10, Y: 10}, 20, 0, rl.RayWhite)
+		r.handleKeyPressed(rl.GetCharPressed(), &rope)
+		text := rope.String()
+		if int(rl.GetTime()*2)%2 == 0 {
+			rl.DrawText("_", r.X, r.Y, 20, rl.RayWhite)
+		}
+
+		rl.DrawTextEx(font, text, rl.Vector2{X: 10, Y: 10}, 20, 0, rl.RayWhite)
 		rl.EndDrawing()
 	}
 }
